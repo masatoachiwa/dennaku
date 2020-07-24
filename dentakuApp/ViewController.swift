b31cc02a3fe0cd59315ab3e571845cbdd6fddfcc
阿知波雅人 <tokemdokea@yahoo.co.jp>
2020-07-25 02:25:32 +0900

    Initial Commit

---
+++ dentakuApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dentakuApp
+//
+//  Created by アプリ開発 on 2020/07/25.
+//  Copyright © 2020 Masato.achiwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+        override func viewDidLoad() {
+                super.viewDidLoad()
+                // Do any additional setup after loading the view.
+        }
+
+
+}
+
